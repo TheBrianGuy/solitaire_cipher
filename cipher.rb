cc9a567eba9ab48d2c797a569fef73060769571f
Brian Hendrickson <brian.hendrickson@live.com>
2016-09-13 12:50:45 -0700

    Add function to convert letters to numbers as per step 3 on quiz

--- cipher.rb
+++ cipher.rb
@@ -1,5 +1,5 @@
 message = "This is a test of the cipher!"
-m_conv_upcase = message.split.join.scan(/[a-zA-Z]{1,5}/)
+m_conv_upcase = message.upcase.split.join.scan(/[a-zA-Z]{1,5}/)
 
 def filler(m_conv_upcase)
 	m_conv_upcase.each do |i|
@@ -12,4 +12,27 @@ def filler(m_conv_upcase)
 end
 
 m_conv_filler = filler(m_conv_upcase)
-p m_conv_filler
+ p m_conv_filler
+
+def convert_to_num(m_conv_filler)
+	hash = Hash.new
+	('A'..'Z').map do |i|
+		hash[i] = i.ord - 64
+	end
+
+	array = Array.new
+	array2 = Array.new
+
+#	p m_conv_filler
+	m_conv_filler.each do |i|
+		i.scan(/./) do |x|
+			array << hash[x]
+		end	
+	end
+	array.each_slice(5) { |e| array2 << e }
+	p	array2
+end
+
+converted_to_n = convert_to_num(m_conv_filler)
+#	p m_conv_filler
+# p converted_to_n 
